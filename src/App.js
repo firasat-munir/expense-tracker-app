9eeb253f85cb06c611468394b6894f5bbd3f45d9
Firasat Munir <zapp3r1997@gmail.com>
2022-08-23 17:07:28 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {Header} from "./container/Header"
+import {Balance} from "./container/Balance"
+import { IncomeExpenses } from './container/IncomeExpenses';
+import { TransactionList } from './container/TransactionList';
+import { AddTransaction } from './container/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+    <Header />
+    <div className='container'>
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+    </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
